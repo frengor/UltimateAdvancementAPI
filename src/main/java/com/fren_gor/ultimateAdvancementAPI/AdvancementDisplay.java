62b411107d88d38c2b887133f0cecb1f6b06b8bc
fren_gor <goro@frengor.com>
2021-06-04 22:49:28 +0200

    Add display and frame

---
+++ src/main/java/com/fren_gor/ultimateAdvancementAPI/AdvancementDisplay.java
@@ -0,0 +1,134 @@
+package com.fren_gor.ultimateAdvancementAPI;
+
+import com.fren_gor.ultimateAdvancementAPI.advancement.Advancement;
+import com.fren_gor.ultimateAdvancementAPI.advancement.RootAdvancement;
+import com.fren_gor.ultimateAdvancementAPI.util.AdvancementUtils;
+import lombok.Getter;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.minecraft.server.v1_15_R1.ChatComponentText;
+import net.minecraft.server.v1_15_R1.MinecraftKey;
+import org.apache.commons.lang.Validate;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_15_R1.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class AdvancementDisplay {
+
+    protected final ItemStack icon;
+    @Getter
+    protected final BaseComponent[] chatTitle;
+    @Getter
+    protected final BaseComponent[] chatDescription;
+    @Getter
+    protected final String title;
+    @Getter
+    @Unmodifiable
+    protected final List<String> description;
+    @Getter
+    protected final String compactDescription;
+    @Getter
+    protected final AdvancementFrameType frame;
+    protected final boolean showToast;
+    protected final boolean announceChat;
+    @Getter
+    protected final float x, y;
+
+    public AdvancementDisplay(@NotNull Material icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull String... description) {
+        this(icon, title, frame, showToast, announceChat, x, y, Arrays.asList(description));
+    }
+
+    public AdvancementDisplay(@NotNull Material icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull List<String> description) {
+        this(new ItemStack(Objects.requireNonNull(icon)), title, frame, showToast, announceChat, x, y, description);
+    }
+
+    public AdvancementDisplay(@NotNull ItemStack icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull String... description) {
+        this(icon, title, frame, showToast, announceChat, x, y, Arrays.asList(description));
+    }
+
+    public AdvancementDisplay(@NotNull ItemStack icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull List<String> description) {
+        Validate.notNull(icon, "Icon is null.");
+        Validate.notNull(title, "Title si null.");
+        Validate.notNull(frame, "Frame si null.");
+        Validate.notNull(description, "Description si null.");
+        Validate.isTrue(isNoElementNull(description), "An element of the description is null.");
+        Validate.isTrue(x >= 0, "x is not null or positive.");
+        Validate.isTrue(y >= 0, "y is not null or positive.");
+
+        this.icon = icon.clone();
+        this.title = title;
+        this.description = Collections.unmodifiableList(description);
+
+        // Remove trailing spaces and color codes
+        String titleTrimmed = title.trim();
+        int toSub = titleTrimmed.length();
+        while (titleTrimmed.charAt(toSub - 2) == '§') {
+            toSub -= 2;
+        }
+        titleTrimmed = titleTrimmed.substring(0, toSub).trim();
+
+        this.chatTitle = TextComponent.fromLegacyText(titleTrimmed);
+        this.chatDescription = AdvancementUtils.fromStringList(title, description);
+
+        StringJoiner joiner = new StringJoiner("\n");
+        for (String s : description)
+            joiner.add(s);
+
+        compactDescription = joiner.toString();
+
+        this.frame = frame;
+        this.showToast = showToast;
+        this.announceChat = announceChat;
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean doesShowToast() {
+        return showToast;
+    }
+
+    public boolean doesAnnounceToChat() {
+        return announceChat;
+    }
+
+    @NotNull
+    public net.minecraft.server.v1_15_R1.AdvancementDisplay getMinecraftDisplay(@NotNull Advancement advancement) {
+        Validate.notNull(advancement, "Advancement is null.");
+        MinecraftKey bg = null;
+        if (advancement instanceof RootAdvancement) {
+            bg = new MinecraftKey(((RootAdvancement) advancement).getBackgroundTexture());
+        }
+
+        net.minecraft.server.v1_15_R1.AdvancementDisplay advDisplay = new net.minecraft.server.v1_15_R1.AdvancementDisplay(getNMSIcon(), new ChatComponentText(title), new ChatComponentText(compactDescription), bg, frame.getMinecraftFrameType(), false, false, false);
+        advDisplay.a(x, y);
+        return advDisplay;
+    }
+
+    @NotNull
+    public ItemStack getIcon() {
+        return icon.clone();
+    }
+
+    @NotNull
+    public net.minecraft.server.v1_15_R1.ItemStack getNMSIcon() {
+        return CraftItemStack.asNMSCopy(icon);
+    }
+
+    private static <T> boolean isNoElementNull(@NotNull List<T> list) {
+        for (T t : list) {
+            if (t == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
