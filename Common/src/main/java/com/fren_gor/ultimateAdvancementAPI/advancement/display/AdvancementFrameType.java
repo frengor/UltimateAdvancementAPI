a2a4db085bdac9f19e1b2734e001b268fdb3ab23
Escanor Targaryen <57372280+EscanorTargaryen@users.noreply.github.com>
2021-08-08 00:26:24 +0200

    Update AdvancementFrameType.java

--- Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/advancement/display/AdvancementFrameType.java
+++ Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/advancement/display/AdvancementFrameType.java
@@ -1,10 +1,15 @@
 package com.fren_gor.ultimateAdvancementAPI.advancement.display;
 
+import com.fren_gor.ultimateAdvancementAPI.advancement.Advancement;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * AdvancementFrameType describe which shape has the advancement
+ */
 @RequiredArgsConstructor
 public enum AdvancementFrameType {
 
@@ -12,13 +17,29 @@ public enum AdvancementFrameType {
     GOAL(net.minecraft.server.v1_15_R1.AdvancementFrameType.GOAL, ChatColor.GREEN, "goal"),
     CHALLENGE(net.minecraft.server.v1_15_R1.AdvancementFrameType.CHALLENGE, ChatColor.DARK_PURPLE, "challenge");
 
+    /**
+     * Real minecraft class for the AdvancementFrameType, it is used in packets
+     */
     @Getter
     private final net.minecraft.server.v1_15_R1.AdvancementFrameType minecraftFrameType;
+
+    /**
+     * Which color is related to the frame, it is used to color the description in hover message in {@link Advancement#getAnnounceMessage(Player)}
+     */
     @Getter
     private final ChatColor color;
+
+    /**
+     * Which world needs to be sent in the description in hover message in {@link Advancement#getAnnounceMessage(Player)}
+     */
     @Getter
     private final String chatText;
 
+    /**
+     * Given a minecraft nms AdvancementFrameType, returns a new AdvancementFrameType
+     * @param nms - nms advancement frame type
+     * @return - AdvancementFrameType
+     */
     @NotNull
     public static AdvancementFrameType fromNMS(@NotNull net.minecraft.server.v1_15_R1.AdvancementFrameType nms) {
         for (AdvancementFrameType a : values()) {
