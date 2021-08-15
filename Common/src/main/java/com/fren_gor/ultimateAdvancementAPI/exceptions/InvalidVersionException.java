752ffa88506fbf6c60ecfcfdc50d1262c1a82c94
fren_gor <goro@frengor.com>
2021-08-16 01:12:37 +0200

    Add version/main thread checks

---
+++ Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/exceptions/InvalidVersionException.java
@@ -0,0 +1,61 @@
+package com.fren_gor.ultimateAdvancementAPI.exceptions;
+
+import jdk.internal.jline.internal.Nullable;
+import lombok.Getter;
+
+public class InvalidVersionException extends RuntimeException {
+
+    @Getter
+    @Nullable
+    private String expected, found;
+
+    public InvalidVersionException() {
+        super();
+    }
+
+    public InvalidVersionException(String message) {
+        super(message);
+    }
+
+    public InvalidVersionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidVersionException(Throwable cause) {
+        super(cause);
+    }
+
+    protected InvalidVersionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public InvalidVersionException(String expected, String found) {
+        super();
+        this.expected = expected;
+        this.found = found;
+    }
+
+    public InvalidVersionException(String expected, String found, String message) {
+        super(message);
+        this.expected = expected;
+        this.found = found;
+    }
+
+    public InvalidVersionException(String expected, String found, String message, Throwable cause) {
+        super(message, cause);
+        this.expected = expected;
+        this.found = found;
+    }
+
+    public InvalidVersionException(String expected, String found, Throwable cause) {
+        super(cause);
+        this.expected = expected;
+        this.found = found;
+    }
+
+    protected InvalidVersionException(String expected, String found, String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        this.expected = expected;
+        this.found = found;
+    }
+}
