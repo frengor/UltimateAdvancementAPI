9cf7f17275887f83688e102a76b227859e37a896
Escanor Targaryen <57372280+EscanorTargaryen@users.noreply.github.com>
2021-08-16 17:41:46 +0200

    Update FancyAdvancementDisplay.java

--- Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/advancement/display/FancyAdvancementDisplay.java
+++ Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/advancement/display/FancyAdvancementDisplay.java
@@ -7,34 +7,59 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.annotation.Inherited;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * FancyAdvancementDisplay is an AdvancementDisplay child class. It has as a difference that puts an empty line between the title and the description in the hover message of the advancement completion message.
+ */
 public class FancyAdvancementDisplay extends AdvancementDisplay {
 
+    /**
+     * The color of the description, if not changed will be gray by default.
+     */
     public static final ChatColor DEFAULT_COLOR = ChatColor.GRAY;
 
+    /**
+     * {@link FancyAdvancementDisplay#FancyAdvancementDisplay(ItemStack, String, AdvancementFrameType, boolean, boolean, float, float, ChatColor, List)}
+     */
     public FancyAdvancementDisplay(@NotNull Material icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull String... description) {
         this(icon, title, frame, showToast, announceChat, x, y, Arrays.asList(description));
     }
 
+    /**
+     * {@link FancyAdvancementDisplay#FancyAdvancementDisplay(ItemStack, String, AdvancementFrameType, boolean, boolean, float, float, ChatColor, List)}
+     */
     public FancyAdvancementDisplay(@NotNull Material icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull List<String> description) {
         this(new ItemStack(Objects.requireNonNull(icon, "Icon is null.")), title, frame, showToast, announceChat, x, y, description);
     }
 
+    /**
+     * {@link FancyAdvancementDisplay#FancyAdvancementDisplay(ItemStack, String, AdvancementFrameType, boolean, boolean, float, float, ChatColor, List)}
+     */
     public FancyAdvancementDisplay(@NotNull ItemStack icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull String... description) {
         this(icon, title, frame, showToast, announceChat, x, y, Arrays.asList(description));
     }
 
+    /**
+     * {@link FancyAdvancementDisplay#FancyAdvancementDisplay(ItemStack, String, AdvancementFrameType, boolean, boolean, float, float, ChatColor, List)}
+     */
     public FancyAdvancementDisplay(@NotNull ItemStack icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull List<String> description) {
         this(icon, title, frame, showToast, announceChat, x, y, DEFAULT_COLOR, description);
     }
 
+    /**
+     * {@link FancyAdvancementDisplay#FancyAdvancementDisplay(ItemStack, String, AdvancementFrameType, boolean, boolean, float, float, ChatColor, List)}
+     */
     public FancyAdvancementDisplay(@NotNull ItemStack icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull ChatColor defaultColor, @NotNull String... description) {
         this(icon, title, frame, showToast, announceChat, x, y, defaultColor, Arrays.asList(description));
     }
 
+    /**
+     * {@link AdvancementDisplay#AdvancementDisplay(ItemStack, String, AdvancementFrameType, boolean, boolean, float, float, ChatColor, List)}
+     */
     public FancyAdvancementDisplay(@NotNull ItemStack icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, ChatColor defaultColor, @NotNull List<String> description) {
         super(icon, title, frame, showToast, announceChat, x, y, defaultColor, description);
 
