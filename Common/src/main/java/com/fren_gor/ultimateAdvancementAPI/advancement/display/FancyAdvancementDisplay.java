f5dda172747ed577f961a5bd5b5fa2f850354ed6
fren_gor <goro@frengor.com>
2021-08-23 00:34:41 +0200

    Add option to customize title color

--- Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/advancement/display/FancyAdvancementDisplay.java
+++ Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/advancement/display/FancyAdvancementDisplay.java
@@ -3,6 +3,7 @@ package com.fren_gor.ultimateAdvancementAPI.advancement.display;
 import com.fren_gor.ultimateAdvancementAPI.util.AdvancementUtils;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.apache.commons.lang.Validate;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
@@ -13,7 +14,8 @@ import java.util.Objects;
 
 public class FancyAdvancementDisplay extends AdvancementDisplay {
 
-    public static final ChatColor DEFAULT_COLOR = ChatColor.GRAY;
+    public static final ChatColor DEFAULT_TITLE_COLOR = ChatColor.WHITE;
+    public static final ChatColor DEFAULT_DESCRIPTION_COLOR = ChatColor.GRAY;
 
     public FancyAdvancementDisplay(@NotNull Material icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull String... description) {
         this(icon, title, frame, showToast, announceChat, x, y, Arrays.asList(description));
@@ -28,20 +30,23 @@ public class FancyAdvancementDisplay extends AdvancementDisplay {
     }
 
     public FancyAdvancementDisplay(@NotNull ItemStack icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull List<String> description) {
-        this(icon, title, frame, showToast, announceChat, x, y, DEFAULT_COLOR, description);
+        this(icon, title, frame, showToast, announceChat, x, y, DEFAULT_TITLE_COLOR, DEFAULT_DESCRIPTION_COLOR, description);
     }
 
-    public FancyAdvancementDisplay(@NotNull ItemStack icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull ChatColor defaultColor, @NotNull String... description) {
-        this(icon, title, frame, showToast, announceChat, x, y, defaultColor, Arrays.asList(description));
+    public FancyAdvancementDisplay(@NotNull ItemStack icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull ChatColor defaultTitleColor, @NotNull ChatColor defaultDescriptionColor, @NotNull String... description) {
+        this(icon, title, frame, showToast, announceChat, x, y, defaultTitleColor, defaultDescriptionColor, Arrays.asList(description));
     }
 
-    public FancyAdvancementDisplay(@NotNull ItemStack icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, ChatColor defaultColor, @NotNull List<String> description) {
-        super(icon, title, frame, showToast, announceChat, x, y, defaultColor, description);
+    public FancyAdvancementDisplay(@NotNull ItemStack icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull ChatColor defaultTitleColor, @NotNull ChatColor defaultDescriptionColor, @NotNull List<String> description) {
+        super(icon, title, frame, showToast, announceChat, x, y, defaultDescriptionColor, description);
+        Validate.notNull(defaultTitleColor, "Default title color is null.");
+
+        this.chatTitle[0] = new TextComponent(defaultTitleColor + rawTitle);
 
         if (compactDescription.isEmpty()) {
-            this.chatDescription[0] = new TextComponent(rawTitle);
+            this.chatDescription[0] = new TextComponent(defaultTitleColor + rawTitle);
         } else {
-            this.chatDescription[0] = new TextComponent(rawTitle + (AdvancementUtils.startsWithEmptyLine(compactDescription) ? "\n" : "\n\n") + compactDescription);
+            this.chatDescription[0] = new TextComponent(defaultTitleColor + rawTitle + (AdvancementUtils.startsWithEmptyLine(compactDescription) ? "\n" : "\n\n") + compactDescription);
         }
     }
 }
