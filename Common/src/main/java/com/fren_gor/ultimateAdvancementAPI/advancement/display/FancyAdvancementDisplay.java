4db733a98c6ec42639d1649b0ca7018025d69ae0
Escanor Targaryen <57372280+EscanorTargaryen@users.noreply.github.com>
2021-08-25 20:19:21 +0200

    Update FancyAdvancementDisplay.java

--- Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/advancement/display/FancyAdvancementDisplay.java
+++ Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/advancement/display/FancyAdvancementDisplay.java
@@ -8,13 +8,14 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.annotation.Inherited;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 /**
- * FancyAdvancementDisplay is an AdvancementDisplay child class. It has as a difference that puts an empty line between the title and the description in the hover message of the advancement completion message.
+ * FancyAdvancementDisplay is an AdvancementDisplay child class. So it has some differences: an empty line between the title and the description in the hover message of the advancement completion message.
+ * And it has a different default title and description colors:
+ * These colors can be set in the constructor or the default ones are: white for the title and gray for the description.
  */
 public class FancyAdvancementDisplay extends AdvancementDisplay {
 
@@ -28,37 +29,101 @@ public class FancyAdvancementDisplay extends AdvancementDisplay {
     public static final ChatColor DEFAULT_DESCRIPTION_COLOR = ChatColor.GRAY;
 
     /**
-     * {@link FancyAdvancementDisplay#FancyAdvancementDisplay(ItemStack, String, AdvancementFrameType, boolean, boolean, float, float, ChatColor, List)}
+     * Create a  new FancyAdvancementDisplay.
+     *
+     * @param icon What item will be shown on the GUI.
+     * @param title The displayed title of the advancement.
+     * @param frame Which shape has the advancement.
+     * @param showToast If it shows the toast message.
+     * @param announceChat If it shows the announceChat message.
+     * @param x X coordinate.
+     * @param y Y coordinate.
+     * @param description The description of the advancement.
      */
     public FancyAdvancementDisplay(@NotNull Material icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull String... description) {
         this(icon, title, frame, showToast, announceChat, x, y, Arrays.asList(description));
     }
 
     /**
-     * {@link FancyAdvancementDisplay#FancyAdvancementDisplay(ItemStack, String, AdvancementFrameType, boolean, boolean, float, float, ChatColor, List)}
+     * Create a  new FancyAdvancementDisplay.
+     *
+     * @param icon What item will be shown on the GUI.
+     * @param title The displayed title of the advancement.
+     * @param frame Which shape has the advancement.
+     * @param showToast If it shows the toast message.
+     * @param announceChat If it shows the announceChat message.
+     * @param x X coordinate.
+     * @param y Y coordinate.
+     * @param description The description of the advancement.
      */
     public FancyAdvancementDisplay(@NotNull Material icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull List<String> description) {
         this(new ItemStack(Objects.requireNonNull(icon, "Icon is null.")), title, frame, showToast, announceChat, x, y, description);
     }
 
     /**
-     * {@link FancyAdvancementDisplay#FancyAdvancementDisplay(ItemStack, String, AdvancementFrameType, boolean, boolean, float, float, ChatColor, List)}
+     * Create a  new FancyAdvancementDisplay.
+     *
+     * @param icon What item will be shown on the GUI.
+     * @param title The displayed title of the advancement.
+     * @param frame Which shape has the advancement.
+     * @param showToast If it shows the toast message.
+     * @param announceChat If it shows the announceChat message.
+     * @param x X coordinate.
+     * @param y Y coordinate.
+     * @param description The description of the advancement.
      */
     public FancyAdvancementDisplay(@NotNull ItemStack icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull String... description) {
         this(icon, title, frame, showToast, announceChat, x, y, Arrays.asList(description));
     }
 
     /**
-     * {@link FancyAdvancementDisplay#FancyAdvancementDisplay(ItemStack, String, AdvancementFrameType, boolean, boolean, float, float, ChatColor, List)}
+     * Create a  new FancyAdvancementDisplay.
+     *
+     * @param icon What item will be shown on the GUI.
+     * @param title The displayed title of the advancement.
+     * @param frame Which shape has the advancement.
+     * @param showToast If it shows the toast message.
+     * @param announceChat If it shows the announceChat message.
+     * @param x X coordinate.
+     * @param y Y coordinate.
+     * @param description The description of the advancement.
      */
     public FancyAdvancementDisplay(@NotNull ItemStack icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull List<String> description) {
         this(icon, title, frame, showToast, announceChat, x, y, DEFAULT_TITLE_COLOR, DEFAULT_DESCRIPTION_COLOR, description);
     }
 
+    /**
+     * Create a  new FancyAdvancementDisplay.
+     *
+     * @param icon What item will be shown on the GUI.
+     * @param title The displayed title of the advancement.
+     * @param frame Which shape has the advancement.
+     * @param showToast If it shows the toast message.
+     * @param announceChat If it shows the announceChat message.
+     * @param x X coordinate.
+     * @param y Y coordinate.
+     * @param defaultTitleColor The default title color.
+     * @param defaultDescriptionColor The default description color.
+     * @param description The description of the advancement.
+     */
     public FancyAdvancementDisplay(@NotNull ItemStack icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull ChatColor defaultTitleColor, @NotNull ChatColor defaultDescriptionColor, @NotNull String... description) {
         this(icon, title, frame, showToast, announceChat, x, y, defaultTitleColor, defaultDescriptionColor, Arrays.asList(description));
     }
 
+    /**
+     * Create a  new FancyAdvancementDisplay.
+     *
+     * @param icon What item will be shown on the GUI.
+     * @param title The displayed title of the advancement.
+     * @param frame Which shape has the advancement.
+     * @param showToast If it shows the toast message.
+     * @param announceChat If it shows the announceChat message.
+     * @param x X coordinate.
+     * @param y Y coordinate.
+     * @param defaultTitleColor The default title color.
+     * @param defaultDescriptionColor The default description color.
+     * @param description The description of the advancement.
+     */
     public FancyAdvancementDisplay(@NotNull ItemStack icon, @NotNull String title, @NotNull AdvancementFrameType frame, boolean showToast, boolean announceChat, float x, float y, @NotNull ChatColor defaultTitleColor, @NotNull ChatColor defaultDescriptionColor, @NotNull List<String> description) {
         super(icon, title, frame, showToast, announceChat, x, y, defaultDescriptionColor, description);
         Validate.notNull(defaultTitleColor, "Default title color is null.");
