46b049d8c3648844fc1e48f36e8890ab3cd46dc9
Escanor Targaryen <57372280+EscanorTargaryen@users.noreply.github.com>
2021-08-26 13:00:21 +0200

    add doc to BaseAdvancement and RootAdvancement classes

--- Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/advancement/BaseAdvancement.java
+++ Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/advancement/BaseAdvancement.java
@@ -2,7 +2,6 @@ package com.fren_gor.ultimateAdvancementAPI.advancement;
 
 import com.fren_gor.ultimateAdvancementAPI.advancement.display.AdvancementDisplay;
 import com.fren_gor.ultimateAdvancementAPI.exceptions.InvalidAdvancementException;
-import lombok.Getter;
 import net.minecraft.server.v1_15_R1.Criterion;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Range;
@@ -14,22 +13,47 @@ import static com.fren_gor.ultimateAdvancementAPI.util.AdvancementUtils.ADV_REWA
 import static com.fren_gor.ultimateAdvancementAPI.util.AdvancementUtils.getAdvancementCriteria;
 import static com.fren_gor.ultimateAdvancementAPI.util.AdvancementUtils.getAdvancementRequirements;
 
+/**
+ * BaseAdvancement directly extends Advancement. It represents an advancement with a parent advancement.
+ */
 public class BaseAdvancement extends Advancement {
 
-    @Getter
+    /**
+     * The advancement parent of the advancement.
+     * <p>The advancement is visually linked with the parent in the advancement GUI.
+     */
     @NotNull
     protected final Advancement parent;
+
     private net.minecraft.server.v1_15_R1.Advancement mcAdvancement;
 
+    /**
+     * Create a new BaseAdvancement.
+     *
+     * @param key The unique key of the advancement.
+     * @param display The {@link AdvancementDisplay} instance of the advancement.
+     * @param parent The advancement parent of the advancement.
+     */
     public BaseAdvancement(@NotNull String key, @NotNull AdvancementDisplay display, @NotNull Advancement parent) {
         this(key, display, parent, 1);
     }
 
+    /**
+     * Create a new BaseAdvancement.
+     *
+     * @param key The unique key of the advancement.
+     * @param display The {@link AdvancementDisplay} instance of the advancement.
+     * @param parent The advancement parent of the advancement.
+     * @param maxCriteria The times the advancement action should be done.
+     */
     public BaseAdvancement(@NotNull String key, @NotNull AdvancementDisplay display, @NotNull Advancement parent, @Range(from = 1, to = Integer.MAX_VALUE) int maxCriteria) {
         super(Objects.requireNonNull(parent, "Parent advancement is null.").advancementTab, key, display, maxCriteria);
         this.parent = parent;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void validateRegister() throws InvalidAdvancementException {
         if (!parent.isValid()) {
@@ -37,6 +61,9 @@ public class BaseAdvancement extends Advancement {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @NotNull
     public net.minecraft.server.v1_15_R1.Advancement getMinecraftAdvancement() {
         if (mcAdvancement != null) {
@@ -45,5 +72,14 @@ public class BaseAdvancement extends Advancement {
 
         Map<String, Criterion> advCriteria = getAdvancementCriteria(maxCriteria);
         return mcAdvancement = new net.minecraft.server.v1_15_R1.Advancement(getMinecraftKey(), parent.getMinecraftAdvancement(), display.getMinecraftDisplay(this), ADV_REWARDS, advCriteria, getAdvancementRequirements(advCriteria));
+    }
+
+    /**
+     * Returns the advancement parent.
+     *
+     * @return The advancement parent.
+     */
+    public Advancement getParent() {
+        return parent;
     }
 }
--- Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/advancement/RootAdvancement.java
+++ Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/advancement/RootAdvancement.java
@@ -1,9 +1,8 @@
 package com.fren_gor.ultimateAdvancementAPI.advancement;
 
-import com.fren_gor.ultimateAdvancementAPI.advancement.display.AdvancementDisplay;
 import com.fren_gor.ultimateAdvancementAPI.AdvancementTab;
+import com.fren_gor.ultimateAdvancementAPI.advancement.display.AdvancementDisplay;
 import com.fren_gor.ultimateAdvancementAPI.database.TeamProgression;
-import lombok.Getter;
 import net.minecraft.server.v1_15_R1.Criterion;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Contract;
@@ -18,22 +17,52 @@ import static com.fren_gor.ultimateAdvancementAPI.util.AdvancementUtils.ADV_REWA
 import static com.fren_gor.ultimateAdvancementAPI.util.AdvancementUtils.getAdvancementCriteria;
 import static com.fren_gor.ultimateAdvancementAPI.util.AdvancementUtils.getAdvancementRequirements;
 
+/**
+ * RootAdvancement directly extends Advancement.
+ * <p>It represents the first advancement of a tab.
+ * <p>Therefore, there must be only one RootAdvancement per tab and it has no parent advancement.
+ * <p>RootAdvancement must be always visible and it contains the background texture path.
+ */
 public class RootAdvancement extends Advancement {
 
-    @Getter
+    /**
+     * The background texture path.
+     * <p>The corresponding image will be used as advancement GUI background.
+     */
     @NotNull
     private final String backgroundTexture;
+
     private net.minecraft.server.v1_15_R1.Advancement mcAdvancement;
 
+    /**
+     * Create a new RootAdvancement.
+     *
+     * @param advancementTab The {@link AdvancementTab} instance that the advancement belongs to.
+     * @param key The unique key of the advancement.
+     * @param display The {@link AdvancementDisplay} instance of the advancement.
+     * @param backgroundTexture The background texture path.
+     */
     public RootAdvancement(@NotNull AdvancementTab advancementTab, @NotNull String key, @NotNull AdvancementDisplay display, @NotNull String backgroundTexture) {
         this(advancementTab, key, display, backgroundTexture, 1);
     }
 
+    /**
+     * Create a new RootAdvancement.
+     *
+     * @param advancementTab The {@link AdvancementTab} instance that the advancement belongs to.
+     * @param key The unique key of the advancement.
+     * @param display The {@link AdvancementDisplay} instance of the advancement.
+     * @param backgroundTexture The background texture path.
+     * @param maxCriteria The times the advancement action should be done.
+     */
     public RootAdvancement(@NotNull AdvancementTab advancementTab, @NotNull String key, @NotNull AdvancementDisplay display, @NotNull String backgroundTexture, @Range(from = 1, to = Integer.MAX_VALUE) int maxCriteria) {
         super(advancementTab, key, display, maxCriteria);
         this.backgroundTexture = Objects.requireNonNull(backgroundTexture, "Background texture is null.");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @NotNull
     public net.minecraft.server.v1_15_R1.Advancement getMinecraftAdvancement() {
         if (mcAdvancement != null) {
@@ -44,21 +73,48 @@ public class RootAdvancement extends Advancement {
         return mcAdvancement = new net.minecraft.server.v1_15_R1.Advancement(getMinecraftKey(), null, display.getMinecraftDisplay(this), ADV_REWARDS, advCrit, getAdvancementRequirements(advCrit));
     }
 
+    /**
+     * The root advancement must be always visible.
+     *
+     * @param player From which player to know whether the advancement is visible to the team.
+     * @return {@code true}.
+     */
     @Override
     @Contract("_ -> true")
     public final boolean isVisible(@NotNull Player player) {
         return true;
     }
 
+    /**
+     * The root advancement must be always visible.
+     *
+     * @param uuid From which UUID player to know whether the advancement is visible to the team.
+     * @return {@code true}.
+     */
     @Override
     @Contract("_ -> true")
     public final boolean isVisible(@NotNull UUID uuid) {
         return true;
     }
 
+    /**
+     * The root advancement must be always visible.
+     *
+     * @param progression From which {@link TeamProgression} to know whether the advancement is visible to the team.
+     * @return {@code true}.
+     */
     @Override
     @Contract("_ -> true")
     public final boolean isVisible(@NotNull TeamProgression progression) {
         return true;
+    }
+
+    /**
+     * Returns the path of the background texture as {@link String}.
+     *
+     * @return The path of the background texture.
+     */
+    public String getBackgroundTexture() {
+        return backgroundTexture;
     }
 }
