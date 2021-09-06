fb48f246457b243100b78a760c0a19c35c4c1090
fren_gor <goro@frengor.com>
2021-09-07 01:59:08 +0200

    Add javadoc

--- Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/database/ObjectResult.java
+++ Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/database/ObjectResult.java
@@ -6,39 +6,92 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
+/**
+ * The {@code ObjectResult} class represents the result of an operation that returns a value, called result. It can either succeed or fail.
+ * If it succeeded, then a (possibly {@code null}) result is provided.
+ * Otherwise, an {@link Exception} occurred.
+ * Example usage:
+ * <blockquote><pre>
+ *     ObjectResult&lt;Integer&gt; res = doOperation();
+ *     if (res.isSucceeded()) { // the condition here is equivalent to !res.isExceptionOccurred()
+ *         // success
+ *         Integer value = res.getResult();
+ *     } else {
+ *         // exception occurred!
+ *         Exception exception = res.getOccurredException();
+ *     }
+ * </pre></blockquote>
+ *
+ * @param <T> The result type
+ */
 public class ObjectResult<T> extends Result {
 
+    /**
+     * The result.
+     */
     protected final T result;
 
+    /**
+     * Creates a successful {@code ObjectResult} with {@code null} result.
+     */
     public ObjectResult() {
         this((T) null);
     }
 
+    /**
+     * Creates a successful {@code ObjectResult} with a result.
+     *
+     * @param result The result (can be {@code null}).
+     */
     public ObjectResult(T result) {
         super();
         this.result = result;
     }
 
+    /**
+     * Creates a failed {@code ObjectResult}.
+     *
+     * @param occurredException The exception occurred during the operation.
+     */
     public ObjectResult(@NotNull Exception occurredException) {
         super(occurredException);
         this.result = null;
     }
 
+    /**
+     * Whether the operation succeeded and the result is not {@code null}.
+     * <p>More formally, this method returns true if and only if {@code (isSucceeded() &amp;&amp; getResult() != null) == true}.
+     *
+     * @return Whether the operation succeeded and the result is not {@code null}.
+     */
     public boolean hasResult() {
-        return result != null;
+        return isSucceeded() && result != null;
     }
 
+    /**
+     * Gets the result of the operation if no {@link Exception} occurred.
+     * If an exception occurred, then it is rethrown as an {@link UnhandledException}.
+     *
+     * @return The result of the operation.
+     * @throws UnhandledException If an exception occurred.
+     */
     @Contract(pure = true)
     public T getResult() throws UnhandledException {
         rethrowExceptionIfOccurred();
         return result;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return "ObjectResult{" + (isExceptionOccurred() ? "occurredException=" + occurredException + ", succeeded=false" : "result=" + result + ", succeeded=true") + '}';
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -50,6 +103,9 @@ public class ObjectResult<T> extends Result {
         return Objects.equals(result, that.result);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         int result1 = super.hashCode();
--- Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/database/Result.java
+++ Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/database/Result.java
@@ -8,29 +8,77 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
+/**
+ * The {@code Result} class represents the result of an operation. It can either succeed or fail.
+ * If it failed, then an {@link Exception} occurred.
+ * Example usage:
+ * <blockquote><pre>
+ *     Result res = doOperation();
+ *     if (res.isSucceeded()) { // the condition here is equivalent to !res.isExceptionOccurred()
+ *         // success
+ *     } else {
+ *         // exception occurred!
+ *         Exception exception = res.getOccurredException();
+ *     }
+ * </pre></blockquote>
+ */
 public class Result {
 
+    /**
+     * A successful {@code Result}.
+     */
     public static final Result SUCCESSFUL = new Result();
 
+    /**
+     * The occurred exception. {@code null} if the operation succeed.
+     */
     protected final Exception occurredException;
 
+    /**
+     * Creates a new successful {@code Result}.
+     */
     public Result() {
         this.occurredException = null;
     }
 
+    /**
+     * Creates a failed {@code Result}.
+     *
+     * @param occurredException The exception occurred during the operation.
+     * @throws IllegalArgumentException If {@code occurredException} is {@code null}.
+     */
     public Result(@NotNull Exception occurredException) {
         Validate.notNull(occurredException, "Exception is null.");
         this.occurredException = occurredException;
     }
 
+    /**
+     * Whether an {@link Exception} occurred executing the operation.
+     * If no {@link Exception} occurred, then the operation succeeded.
+     *
+     * @return {@code true} if no exception occurred, {@code false} otherwise.
+     */
     public boolean isExceptionOccurred() {
         return occurredException != null;
     }
 
+    /**
+     * Whether the operation succeeded.
+     * If the operation succeeded, then no {@link Exception} occurred.
+     *
+     * @return {@code true} if the operation succeeded, {@code false} otherwise.
+     */
     public boolean isSucceeded() {
         return occurredException == null;
     }
 
+    /**
+     * Gets the occurred {@link Exception}.
+     * An {@link IllegalOperationException} is thrown if no exception occurred.
+     *
+     * @return The occurred exception.
+     * @throws IllegalOperationException If no exception occurred.
+     */
     public Exception getOccurredException() throws IllegalOperationException {
         if (!isExceptionOccurred()) {
             throw new IllegalOperationException("No exception occurred.");
@@ -38,17 +86,35 @@ public class Result {
         return occurredException;
     }
 
+    /**
+     * Rethrow the occurred exception as an {@link UnhandledException}.
+     * An {@link IllegalOperationException} is thrown if no exception occurred.
+     *
+     * @throws UnhandledException If an exception occurred.
+     * @throws IllegalOperationException If no exception occurred.
+     */
     @Contract("-> fail")
     public void rethrowException() throws UnhandledException, IllegalOperationException {
         rethrowExceptionIfOccurred();
         throw new IllegalOperationException("No exception occurred.");
     }
 
+    /**
+     * Rethrow the occurred exception as an {@link UnhandledException} only if that occurred.
+     *
+     * @throws UnhandledException If an exception occurred.
+     */
     public void rethrowExceptionIfOccurred() throws UnhandledException {
         if (isExceptionOccurred())
             throw new UnhandledException(occurredException);
     }
 
+    /**
+     * Prints the stack trace of the occurred exception.
+     * An {@link IllegalOperationException} is thrown if no exception occurred.
+     *
+     * @throws IllegalOperationException If no exception occurred.
+     */
     public void printStackTrace() throws IllegalOperationException {
         if (!isExceptionOccurred()) {
             throw new IllegalOperationException("No exception occurred.");
@@ -56,11 +122,17 @@ public class Result {
         occurredException.printStackTrace();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return "Result{" + (isExceptionOccurred() ? "occurredException=" + occurredException + ", succeeded=false" : "succeeded=true") + '}';
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -71,6 +143,9 @@ public class Result {
         return Objects.equals(occurredException, result.occurredException);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         return occurredException != null ? occurredException.hashCode() : 0;
