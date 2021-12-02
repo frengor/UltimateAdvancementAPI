d5f14519f86e645eeb3b337f13e2467f299e78c6
fren_gor <goro@frengor.com>
2021-12-02 02:03:36 +0100

    Add 1.18 support

---
+++ NMS/1_18_R1/src/main/java/com/fren_gor/ultimateAdvancementAPI/nms/v1_18_R1/Util.java
@@ -0,0 +1,72 @@
+package com.fren_gor.ultimateAdvancementAPI.nms.v1_18_R1;
+
+import com.google.common.collect.Maps;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Criterion;
+import net.minecraft.advancements.CriterionProgress;
+import net.minecraft.advancements.critereon.ImpossibleTrigger;
+import net.minecraft.network.protocol.Packet;
+import org.apache.commons.lang.Validate;
+import org.bukkit.craftbukkit.v1_18_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+import java.util.Map;
+
+public class Util {
+
+    @NotNull
+    public static Map<String, Criterion> getAdvancementCriteria(@Range(from = 1, to = Integer.MAX_VALUE) int maxProgression) {
+        Validate.isTrue(maxProgression >= 1, "Max progression must be >= 1.");
+
+        Map<String, Criterion> advCriteria = Maps.newHashMapWithExpectedSize(maxProgression);
+        for (int i = 0; i < maxProgression; i++) {
+            advCriteria.put(String.valueOf(i), new Criterion(new ImpossibleTrigger.TriggerInstance()));
+        }
+
+        return advCriteria;
+    }
+
+    @NotNull
+    public static String[][] getAdvancementRequirements(@NotNull Map<String, Criterion> advCriteria) {
+        Validate.notNull(advCriteria, "Advancement criteria map is null.");
+
+        String[][] array = new String[advCriteria.size()][1];
+        int index = 0;
+        for (String name : advCriteria.keySet()) {
+            array[index++][0] = name;
+        }
+
+        return array;
+    }
+
+    @NotNull
+    public static AdvancementProgress getAdvancementProgress(@NotNull Advancement mcAdv, @Range(from = 0, to = Integer.MAX_VALUE) int progression) {
+        Validate.notNull(mcAdv, "NMS Advancement is null.");
+        Validate.isTrue(progression >= 0, "Progression must be >= 0.");
+
+        AdvancementProgress advPrg = new AdvancementProgress();
+        advPrg.update(mcAdv.getCriteria(), mcAdv.getRequirements());
+
+        for (int i = 0; i < progression; i++) {
+            CriterionProgress criteriaPrg = advPrg.getCriterion(String.valueOf(i));
+            if (criteriaPrg != null) {
+                criteriaPrg.grant();
+            }
+        }
+
+        return advPrg;
+    }
+
+    public static void sendTo(@NotNull Player player, @NotNull Packet<?> packet) {
+        Validate.notNull(player, "Player is null.");
+        Validate.notNull(packet, "Packet is null.");
+        ((CraftPlayer) player).getHandle().connection.send(packet);
+    }
+
+    private Util() {
+        throw new UnsupportedOperationException("Utility class.");
+    }
+}
