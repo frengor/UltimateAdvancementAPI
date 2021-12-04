48f2d9afbfc102e86743adf1816847b6d2b522d8
fren_gor <goro@frengor.com>
2021-12-04 23:13:27 +0100

    Use CommandAPI 6.5.2 for MC 1.18

---
+++ Commands/CommandAPI-6.5.2/src/main/java/com/fren_gor/ultimateAdvancementAPI/commands/commandAPI_v6_5_2/CommandAPIManager_v6_5_2.java
@@ -0,0 +1,26 @@
+package com.fren_gor.ultimateAdvancementAPI.commands.commandAPI_v6_5_2;
+
+import com.fren_gor.ultimateAdvancementAPI.AdvancementMain;
+import com.fren_gor.ultimateAdvancementAPI.commands.CommandAPIManager.*;
+import dev.jorel.commandapi.CommandAPI;
+import dev.jorel.commandapi.CommandAPIConfig;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+public class CommandAPIManager_v6_5_2 implements ILoadable {
+
+    public CommandAPIManager_v6_5_2() {
+    }
+
+    @Override
+    public void onLoad(@NotNull AdvancementMain main) {
+        CommandAPI.onLoad(new CommandAPIConfig().verboseOutput(false).silentLogs(true));
+
+        new UltimateAdvancementAPICommand_v6_5_2(main).register();
+    }
+
+    @Override
+    public void onEnable(@NotNull Plugin plugin) {
+        CommandAPI.onEnable(plugin);
+    }
+}
--- Commands/Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/commands/CommandAPIVersion.java
+++ Commands/Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/commands/CommandAPIVersion.java
@@ -13,6 +13,7 @@ import java.util.List;
 public enum CommandAPIVersion {
     v5_12("5.12", "lCfbzi9Nf/RzQl6GvSGU/DqjuxkGDWavTNXt6Vms50o=", "5_12", List.of("v1_15_R1", "v1_16_R1", "v1_16_R2", "v1_16_R3")),
     v6_4_0("6.4.0", "Cdh9pugQD7LDrzKNUThn5WM0l2yhFRQb8iE9Y4dzvM8=", "6_4_0", List.of("v1_17_R1")),
+    v6_5_2("6.5.2", "bCRRXGp/Ff764Mk6Rh+1U+DP4ucEmSVPJddgcufHxow=", "6_5_2", List.of("v1_18_R1"));
 
     private final String version, checksum, suffix;
     private final List<String> supportedVersions;
