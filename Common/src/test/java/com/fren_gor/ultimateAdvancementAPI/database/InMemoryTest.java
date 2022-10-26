4ff06934498b4a675c2e3a73ee25ce5a49385b6b
fren_gor <goro@frengor.com>
2022-10-26 12:29:29 +0200

    Test that in-memory db is working correctly during tests
    This is useful to be more confident that a failure of a DatabaseManager's test is not caused by SQLite messing up. This is not intended to test the `InMemory` class, but only to make sure that the database is set up correctly during tests.

--- Common/src/test/java/com/fren_gor/ultimateAdvancementAPI/database/InMemoryTest.java
+++ Common/src/test/java/com/fren_gor/ultimateAdvancementAPI/database/InMemoryTest.java
@@ -1,17 +1,50 @@
 package com.fren_gor.ultimateAdvancementAPI.database;
 
 import com.fren_gor.ultimateAdvancementAPI.database.impl.InMemory;
+import com.fren_gor.ultimateAdvancementAPI.util.AdvancementKey;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+import java.util.UUID;
 import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class InMemoryTest {
 
+    private InMemory db;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        db = new InMemory(Logger.getLogger("InMemoryTest"));
+        db.setUp();
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        db.close();
+        db = null;
+    }
+
     @Test
     public void inMemoryTest() throws Exception {
-        InMemory m = new InMemory(Logger.getLogger("InMemoryTest"));
-        m.setUp();
-        m.clearUpTeams();
-        m.close();
+        UUID uuid = UUID.randomUUID();
+        var res = db.loadOrRegisterPlayer(uuid, "Dummy");
+        assertTrue(res.getKey().contains(uuid));
+        assertTrue(res.getKey().everyMemberMatch(u -> u.equals(uuid)));
+        assertTrue(res.getValue()); // This is a new team
+
+        AdvancementKey key = new AdvancementKey("test", "adv");
+        db.updateAdvancement(key, res.getKey().getTeamId(), 10);
+
+        boolean executedAtLeastOnce = false;
+        for (var entry : db.getTeamAdvancements(res.getKey().getTeamId()).entrySet()) {
+            executedAtLeastOnce = true;
+            assertEquals(key, entry.getKey());
+            assertEquals(10, entry.getValue());
+        }
+        assertTrue(executedAtLeastOnce);
     }
 }
