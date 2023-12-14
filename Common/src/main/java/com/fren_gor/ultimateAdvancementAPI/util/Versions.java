2c16241c4b4e972500770a80517a4f29328a4417
Escanor Targaryen <57372280+EscanorTargaryen@users.noreply.github.com>
2023-12-14 01:27:26 +0100

    Update to minecraft 1.20.3-1-20.4

--- Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/util/Versions.java
+++ Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/util/Versions.java
@@ -29,7 +29,8 @@ public class Versions {
             "v1_19_R2",
             "v1_19_R3",
             "v1_20_R1",
-            "v1_20_R2"
+            "v1_20_R2",
+            "v1_20_R3"
     );
 
     private static final Map<String, List<String>> NMS_TO_VERSIONS = Map.ofEntries(
@@ -44,7 +45,8 @@ public class Versions {
             Map.entry("v1_19_R2", List.of("1.19.3")),
             Map.entry("v1_19_R3", List.of("1.19.4")),
             Map.entry("v1_20_R1", List.of("1.20", "1.20.1")),
-            Map.entry("v1_20_R2", List.of("1.20.2"))
+            Map.entry("v1_20_R2", List.of("1.20.2")),
+            Map.entry("v1_20_R3", List.of("1.20.3", "1.20.4"))
     );
 
     private static final Map<String, String> NMS_TO_FANCY = Map.ofEntries(
@@ -59,7 +61,8 @@ public class Versions {
             Map.entry("v1_19_R2", "1.19.3"),
             Map.entry("v1_19_R3", "1.19.4"),
             Map.entry("v1_20_R1", "1.20-1.20.1"),
-            Map.entry("v1_20_R2", "1.20.2")
+            Map.entry("v1_20_R2", "1.20.2"),
+            Map.entry("v1_20_R3", "1.20.3-1.20.4")
     );
 
     private static final List<String> SUPPORTED_VERSIONS = SUPPORTED_NMS_VERSIONS.stream()
