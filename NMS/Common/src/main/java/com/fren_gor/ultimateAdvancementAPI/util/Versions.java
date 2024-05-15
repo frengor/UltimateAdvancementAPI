5c6c1f35dfcd1e4c703bbcda490b6702c017b697
fren_gor <goro@frengor.com>
2024-05-15 18:56:04 +0200

    Update version to 2.3.1

--- NMS/Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/util/Versions.java
+++ NMS/Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/util/Versions.java
@@ -16,7 +16,7 @@ import java.util.Optional;
  */
 public class Versions {
 
-    private static final String API_VERSION = "2.3.0";
+    private static final String API_VERSION = "2.3.1";
 
     private static final List<String> SUPPORTED_NMS_VERSIONS = List.of(
             "v1_15_R1",
