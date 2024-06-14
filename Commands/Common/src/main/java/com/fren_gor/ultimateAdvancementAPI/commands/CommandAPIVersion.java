12b9d0c32bd2651ff60c27fd385d62d57e1eb040
Escanor Targaryen <57372280+EscanorTargaryen@users.noreply.github.com>
2024-06-14 17:37:21 +0200

    Update to Minecraft 1.21

--- Commands/Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/commands/CommandAPIVersion.java
+++ Commands/Common/src/main/java/com/fren_gor/ultimateAdvancementAPI/commands/CommandAPIVersion.java
@@ -32,7 +32,8 @@ public enum CommandAPIVersion {
                     "v1_20_R1",
                     "v1_20_R2",
                     "v1_20_R3",
-                    "v1_20_R4"
+                    "v1_20_R4",
+                    "v1_21_R1"
             )
     );
 
