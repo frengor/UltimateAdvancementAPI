c032818500544a4b4da6bafa062cc36f9295eaae
fren_gor <goro@frengor.com>
2024-11-08 04:03:04 +0100

    Update dev-3.0.0 to 2.4.2

--- NMS/1_21_R2/src/main/java/com/fren_gor/ultimateAdvancementAPI/nms/v1_21_R2/advancement/AdvancementDisplayWrapper_v1_21_R2.java
+++ NMS/1_21_R2/src/main/java/com/fren_gor/ultimateAdvancementAPI/nms/v1_21_R2/advancement/AdvancementDisplayWrapper_v1_21_R2.java
@@ -5,6 +5,7 @@ import com.fren_gor.ultimateAdvancementAPI.nms.wrappers.advancement.AdvancementD
 import com.fren_gor.ultimateAdvancementAPI.nms.wrappers.advancement.AdvancementFrameTypeWrapper;
 import net.minecraft.advancements.AdvancementType;
 import net.minecraft.advancements.DisplayInfo;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import org.bukkit.craftbukkit.v1_21_R2.inventory.CraftItemStack;
 import org.bukkit.craftbukkit.v1_21_R2.util.CraftChatMessage;
@@ -22,6 +23,13 @@ public class AdvancementDisplayWrapper_v1_21_R2 extends AdvancementDisplayWrappe
     public AdvancementDisplayWrapper_v1_21_R2(@NotNull ItemStack icon, @NotNull String title, @NotNull String description, @NotNull AdvancementFrameTypeWrapper frameType, float x, float y, boolean showToast, boolean announceChat, boolean hidden, @Nullable String backgroundTexture) {
         ResourceLocation background = backgroundTexture == null ? null : ResourceLocation.parse(backgroundTexture);
         this.display = new DisplayInfo(CraftItemStack.asNMSCopy(icon), Util.fromString(title), Util.fromString(description), Optional.ofNullable(background), (AdvancementType) frameType.toNMS(), showToast, announceChat, hidden);
+        this.display.setLocation(x, y);
+        this.frameType = frameType;
+    }
+
+    protected AdvancementDisplayWrapper_v1_21_R2(@NotNull net.minecraft.world.item.ItemStack icon, @NotNull Component title, @NotNull Component description, @NotNull AdvancementFrameTypeWrapper frameType, float x, float y, boolean showToast, boolean announceChat, boolean hidden, @Nullable String backgroundTexture) {
+        ResourceLocation background = backgroundTexture == null ? null : ResourceLocation.parse(backgroundTexture);
+        this.display = new DisplayInfo(icon, title, description, Optional.ofNullable(background), (AdvancementType) frameType.toNMS(), showToast, announceChat, hidden);
         this.display.setLocation(x, y);
         this.frameType = frameType;
     }
