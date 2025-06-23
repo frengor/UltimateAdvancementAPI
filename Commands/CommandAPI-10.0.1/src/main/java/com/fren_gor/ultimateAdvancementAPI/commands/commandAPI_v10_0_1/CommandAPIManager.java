ecfbe232dc939c51fd1268329f632261e553923b
Davide Bilardello <57372280+EscanorTargaryen@users.noreply.github.com>
2025-06-23 22:14:41 +0200

    Set CommandAPI namespace
    Fix https://discord.com/channels/876458743934648340/1373045521660379207

--- Commands/CommandAPI-10.0.1/src/main/java/com/fren_gor/ultimateAdvancementAPI/commands/commandAPI_v10_0_1/CommandAPIManager.java
+++ Commands/CommandAPI-10.0.1/src/main/java/com/fren_gor/ultimateAdvancementAPI/commands/commandAPI_v10_0_1/CommandAPIManager.java
@@ -16,9 +16,9 @@ public class CommandAPIManager implements ILoadable {
     public void onLoad(@NotNull AdvancementMain main, @NotNull JavaPlugin plugin) {
         CommandAPI.onLoad(
                 new CommandAPIBukkitConfig(plugin)
-                .verboseOutput(false)
-                .silentLogs(true)
-                .usePluginNamespace()
+                        .verboseOutput(false)
+                        .silentLogs(true)
+                        .setNamespace("UltimateAdvancementAPI")
         );
 
         new UltimateAdvancementAPICommand(main).register();
